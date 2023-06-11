b2bf499ece1905fb245840e7819f01ba13a1d200
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-06-11 11:22:13 +0600

    style updated

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -55,13 +55,13 @@ const Navbar = () => {
     }, []);
 
     return (
-        <div className="navbar flex justify-between fixed z-10 opacity-30 text-white bg-black max-w-screen-xl">
+        <div className="navbar flex justify-between fixed z-10 opacity-70 text-4xl font-bold text-white bg-blue-800 max-w-screen-xl">
             <div className="navbar-start text-white">
-                <div className="dropdown text-white">
+                <div className="dropdown text-white bg-blue-800 opacity-70">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-blue-800 rounded-box w-52">
                         {options}
                     </ul>
                 </div>
